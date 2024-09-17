0e6c2243bcc60e0e84980bbef29dda90ddb7789c
LilDavid <dat.dotuan@hcmut.edu.vn>
2024-09-17 10:14:58 +0700

    auto commit

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const { createServer } = require('node:http');
+// const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Đoạn trên là chưa dùng thêm framework express ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+const express = require('express')  
+const app = express()       // app express 
+const port = 3000           // port
+
+// Khai báo route
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log('Example app listening on port 3000:')
+})
