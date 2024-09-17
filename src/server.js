0a05a52b99e37b7adbac05b53654dab4f6f27c6a
LilDavid <dat.dotuan@hcmut.edu.vn>
2024-09-17 20:31:46 +0700

    auto commit

--- src/server.js
+++ src/server.js
@@ -18,13 +18,27 @@
 
 
 
-const express = require('express')  
+const express = require('express')   // commonjs
 const app = express()       // app express 
-const port = 3000           // port
+const port = 3000           // port   ==> hardcode 
+const path = require('path')    // cách để sử dụng 1 thư viện    // commonjs
+
+
+//config template engine
+//app.set('view', './views/')      // './src/views'  khi mà cái server này nằm ngoài src        ==> Sau đấy ta thêm 1 dấu gạch để nó tìm kiếm TRONG thư mục view.
+app.set('views', path.join(__dirname, 'views'));                     //path.join giúp t tìm đc đường dẫn đến 1 file trực tiếp (và nó là 1 thư viện)
+app.set('view engine', 'ejs')
+
+
 
 // Khai báo route
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/dat', (req, res) => {
+  // res.send('<h1>DAT</h1>')
+  res.render('sample.ejs')
 })
 
 app.listen(port, () => {
