98739cb80cd1424a4025a806f1966fbc30a77278
LilDavid <dat.dotuan@hcmut.edu.vn>
2024-09-20 00:34:47 +0700

    auto commit

--- src/server.js
+++ src/server.js
@@ -16,8 +16,6 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Đoạn trên là chưa dùng thêm framework express ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-
-
 const express = require('express'); // commonjs
 const app = express();       // app express                // CHỖ NÀY LÀ DUY NHẤT TRONG 1 PROJECT? NÊN Ở web.js mới ko đc có thêm app này mà thay bằng route
 require('dotenv').config();   // Câu lệnh này để sử dụng dotenv
@@ -31,16 +29,14 @@ const configViewengine = require('./config/viewEngine');
 
 const webRoutes = require('./routes/web')    // ở đây ko cần đặt đúng tên router như đã export ở web.js vì file đó nó chỉ xuất ra 1 biến
 
-
+/*
 //app.set('view', './views/')      // './src/views'  khi mà cái server này nằm ngoài src        ==> Sau đấy ta thêm 1 dấu gạch để nó tìm kiếm TRONG thư mục view.
 //app.set('views', path.join(__dirname, 'views'));                     //path.join giúp t tìm đc đường dẫn đến 1 file trực tiếp (và nó là 1 thư viện)
 //app.set('view engine', 'ejs');
-
+*/
 // CONFIG TEMPLATE ENGINE
 configViewengine(app);
-
-
-
+/*
 // console.log(">>> check env: ", process.env);
 
 //config static files
@@ -57,7 +53,7 @@ configViewengine(app);
 //   // res.send('<h1>DAT</h1>')
 //   res.render('sample.ejs')
 // })
-
+*/
 // KHAI BÁO ROUTE 
 app.use('/', webRoutes);     // vậy tức là ở đây nó sẽ tạo ra một phần tiền tố "/" 
                             // trc mỗi câu lệnh rồi mới dẫn tới mấy cái tiền tố khác để dẫn đến các route. 
